004378afa8fd517f466fe7e632c6c14039fdac4f
Charles Cui <xarlze@ool-ad0205b7.dyn.optonline.net>
2019-04-11 17:40:27 -0400

    modify routing logic

--- imhere_app/src/components/seekerpages/RenderGiver.js
+++ imhere_app/src/components/seekerpages/RenderGiver.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+
+const seekerid=1;
+const giverid=1;
 
 export default class RenderGiver extends Component {
   render() {
     return (
       <div>
-        
+        <Link to={`/seeker/${seekerid}/search/${giverid}/request`}>Request</Link>
       </div>
     )
   }
