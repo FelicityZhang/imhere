7bb2f466f3852e60114c288a5b376d77ec5355ce
Charles Cui <xarlze@ool-ad0205b7.dyn.optonline.net>
2019-04-11 15:05:23 -0400

    correct package.json

--- imhere_app/src/components/Landing.js
+++ imhere_app/src/components/Landing.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 export default class Landing extends Component {
   render() {
     return (
       <div>
-        
+        <h2>I'm a...</h2>
+        <Link to='/seeker'>Seeker</Link>
+        <Link to='/seeker'>Giver</Link>
       </div>
     )
   }
