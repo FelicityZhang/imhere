ed29adbe2e8bb1c9bbbfca7efdbeab97bd39336c
felicityzhang <xz1574@nyu.edu>
2019-04-12 09:25:03 -0400

    sign page

--- imhere_app/src/components/seekerpages/SeekerSign.js
+++ imhere_app/src/components/seekerpages/SeekerSign.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class SeekerSign extends Component {
+  constructor ( props ) {
+    super( props )
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.handleSeekerSignInfo = this.handleSeekerSignInfo.bind( this );
+    this.handleSeekerSignSubmit = this.handleSeekerSignSubmit.bind( this );
+
+  }
+
+  handleSeekerSignInfo( event ) {
+    this.setState( {
+      [ event.target.name ]: event.target.value
+    } )
+  }
+
+  handleSeekerSignSubmit( event ) {
+    event.preventDefault();
+    const { email, password } = this.state;
+    const data = { email, password }
+    this.props.seekerSign( data )
+    this.setState( {
+      email: '',
+      password: ''
+    } )
+  }
+
+
+
   render() {
     return (
-      <div>
-        
+      <div className='seekerSign'>
+        <h1>Sign</h1>
+        <form>
+          <input
+            name='email'
+            placeholder='email'
+            value={ this.state.email }
+            onChange={ this.handleSeekerSignInfo }
+          />
+          <br />
+          <input
+            name='password'
+            placeholder='password'
+            value={ this.state.password }
+            onChange={ this.handleSeekerSignInfo }
+          />
+          <br />
+          <button
+            onClick={ ( event ) => this.handleSeekerSignSubmit( event ) }>
+            Submit
+          </button>
+        </form>
       </div>
     )
   }
+
+
 }
+export default SeekerSign;
