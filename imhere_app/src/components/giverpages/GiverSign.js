59828aaa3fcfdc7f92faf7622527e4ca4e35998c
Your Name <you@example.com>
2019-04-12 13:50:09 -0400

    updated Seeker request form file

--- imhere_app/src/components/giverpages/GiverSign.js
+++ imhere_app/src/components/giverpages/GiverSign.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-export default class GiverSign extends Component {
+class GiverSign extends Component {
   constructor ( props ) {
     super( props )
     this.state = {
