ea12aabc3c502a063a83170ab3ba17a50ab4c55e
Charles Cui <xarlze@ool-ad0205b7.dyn.optonline.net>
2019-04-12 10:01:42 -0400

    add landing css

--- imhere_app/src/components/Landing.js
+++ imhere_app/src/components/Landing.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 
+import './Landing.css';
+
 export default class Landing extends Component {
   render() {
     return (
       <div>
         <h2>I'm a...</h2>
-        <Link to='/seeker'>Seeker</Link>
+        <Link to='/seeker'>
+          <div id="selectSeeker">
+            Seeker
+          </div>
+        </Link>
         <Link to='/giver'>Giver</Link>
       </div>
     )
