376ea66c75d369cc545d50e1a3cde0b9bee442e5
Charles Cui <xarlze@ool-ad0205b7.dyn.optonline.net>
2019-04-12 14:23:52 -0400

    landing page animation

--- imhere_app/src/components/giverpages/GiverLogin.js
+++ imhere_app/src/components/giverpages/GiverLogin.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
-export default class GiverLogin extends Component {
+class GiverLogin extends Component {
   render() {
     return (
       <div>
+        <div
+          id="reg"
+        >Create An Account</div>
+        <div
+          id="sign"
+        >Sign In</div>
         <Link to='/giver/registration'>Create An Account</Link>
         <Link to='/giver/signin'>Sign In</Link>
       </div>
     )
   }
 }
+export default withRouter(GiverLogin);
