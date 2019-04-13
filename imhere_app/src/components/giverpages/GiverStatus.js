a305d26d36ae7c467d1b2949e20bd7d2c0525c98
Your Name <you@example.com>
2019-04-13 16:02:20 -0400

    giverstatus page

--- imhere_app/src/components/giverpages/GiverStatus.js
+++ imhere_app/src/components/giverpages/GiverStatus.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class GiverStatus extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    render() {
+      const requests = this.props.requests.map(request => {
+        console.log(request)
+        return(
+          <div>
+              <span> Description </span>
+              <h2>{request.description}></h2>
+                <br/>
+              <span> Project Begins </span>
+              <h2>{request.start_time}></h2>
+                <br/>
+              <span> Project End</span>
+              <h2>{request.title}></h2>
+              <h2> Requested Service</h2>
+          </div>
+        )
+      })
+      return(
+        {requests}
+      )
   }
 }
